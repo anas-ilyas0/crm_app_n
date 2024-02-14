8d6b0dbdc4186701ef4b19414aa5814fec6f2d47
MUHAMMAD ANAS FAIZAN <manasfaizan0786@gmail.com>
2024-02-14 20:05:15 +0500

    changes

--- lib/providers/user_provider.dart
+++ lib/providers/user_provider.dart
@@ -1,41 +1,41 @@
-import 'dart:io';
+//mport 'dart:io';
 import 'package:flutter/material.dart';
 
 class UserProvider with ChangeNotifier {
-  String name, email, address, contactType;
-  String receivedData;
-  File? image;
-  bool dataAvailable = false;
+  // String name, email, address, contactType;
+  // String receivedData;
+  // File? image;
+  // bool dataAvailable = false;
   bool _isLoading = false;
   bool get isLoading => _isLoading;
 
-  UserProvider({
-    this.name = '',
-    this.email = '',
-    this.address = '',
-    this.contactType = '',
-    this.receivedData = '',
-    this.image,
-  });
+  // UserProvider({
+  //   this.name = '',
+  //   this.email = '',
+  //   this.address = '',
+  //   this.contactType = '',
+  //   this.receivedData = '',
+  //   this.image,
+  // });
 
   void setLoading(bool loading) {
     _isLoading = loading;
     notifyListeners();
   }
 
-  void changeData({
-    required String newName,
-    required String newEmail,
-    required String newAddress,
-    required String newContactType,
-    required File? newImage,
-  }) async {
-    name = newName;
-    email = newEmail;
-    address = newAddress;
-    contactType = newContactType;
-    image = newImage;
-    dataAvailable = true;
-    notifyListeners();
-  }
+  // void changeData({
+  //   required String newName,
+  //   required String newEmail,
+  //   required String newAddress,
+  //   required String newContactType,
+  //   required File? newImage,
+  // }) async {
+  //   name = newName;
+  //   email = newEmail;
+  //   address = newAddress;
+  //   contactType = newContactType;
+  //   image = newImage;
+  //   dataAvailable = true;
+  //   notifyListeners();
+  // }
 }
